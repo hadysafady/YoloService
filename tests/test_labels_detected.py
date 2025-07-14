077f46a03a75e347f3fd0bbb336ee60d52fdfa64
hady <hadysfady@gmail.com>
2025-07-14 22:11:30 +0300

    add labels_detected in the last 7 days

---
+++ tests/test_labels_detected.py
@@ -0,0 +1,15 @@
+import unittest
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+class TestLabelsDetected(unittest.TestCase):
+    def test_label_detected_endpoint(self):
+        response = client.get("/labels")
+        self.assertEqual(response.status_code,200)
+        self.assertIsInstance(response.json(),list)
+        self.assertGreater(len(response.json()),0)
+        labels = response.json()
+        for label in labels:
+            self.assertEqual(type(label),str)
