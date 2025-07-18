01b939840bed4706d713397935ceaea306e37d57
hady <hadysfady@gmail.com>
2025-07-19 02:34:18 +0300

    update test_delete_prediction with username

--- tests/test_delete_prediction_uid.py
+++ tests/test_delete_prediction_uid.py
@@ -1,12 +1,18 @@
+import base64
 import unittest
 from fastapi.testclient import TestClient
 from app import app
 
 client = TestClient(app)
 
+def get_basic_auth_header(username, password):
+    token = base64.b64encode(f"{username}:{password}".encode()).decode()
+    return {"Authorization": f"Basic {token}"}
+
 class TestDeletePrediction(unittest.TestCase):
     def test_delete_prediction_endpoint(self):
 
+        headers = get_basic_auth_header("hadyy" , "safadyy")
         with open("323101.jpg", "rb") as f:
             response = client.post("/predict", files={"file": f})
         self.assertEqual(response.status_code, 200)
@@ -15,9 +21,10 @@ class TestDeletePrediction(unittest.TestCase):
         self.assertIsInstance(prediction_uid, str)
 
 
-        delete_response = client.delete(f"/prediction/{prediction_uid}")
+        delete_response = client.delete(f"/prediction/{prediction_uid}",headers=headers)
         self.assertEqual(delete_response.status_code, 200)
         self.assertIn("deleted successfully", delete_response.json()["detail"])
 
-        get_response = client.get(f"/prediction/{prediction_uid}")
-        self.assertEqual(get_response.status_code, 404)
+        get_response = client.get(f"/prediction/{prediction_uid}",headers=headers)
+        self.assertEqual(get_response.status_code, 404 or 401)
+
